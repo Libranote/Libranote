7124ae7d474b6095f35fa7147358742c1ec4212a
Bat <baptiste@gelez.xyz>
2017-11-02 12:57:54 +0100

    File upload

--- libranote/libranote/urls.py
+++ libranote/libranote/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/v1/', include('libranote.api.urls'))
-]
+    url(r'^api/v1/', include('libranote.api.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
